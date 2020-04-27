ee99dfeda9994544f6fad99d253a87c0a1f9335a
SergejMaz <64395909+SergejMaz@users.noreply.github.com>
2020-04-27 21:20:23 +0300

    Create ITekstas.cs

---
+++ ITekstas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestavimuiUZD
+{
+    class ITekstas
+    {
+        string tekstas{ get;  set; }
+        void ras(string rasas)
+        {
+
+        }
+    }
+
+    class ITeks
+    {
+        int sk { get; }
+        double sk1 { get; set; }
+    }
+}
